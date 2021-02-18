c578437161d6889482a44df4d87231ce5ee84cf7
HennaMian <43707978+HennaMian@users.noreply.github.com>
2021-02-18 16:55:56 -0500

    Add files via upload

---
+++ submission/images/missileAway.h
@@ -0,0 +1,24 @@
+//Henna Mian
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 --resize=3x10 missileAway missileAway.png 
+ * Time-stamp: Thursday 03/26/2020, 19:56:40
+ * 
+ * Image Information
+ * -----------------
+ * missileAway.png 3@10
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef MISSILEAWAY_H
+#define MISSILEAWAY_H
+
+extern const unsigned short missileAway[30];
+#define MISSILEAWAY_SIZE 60
+#define MISSILEAWAY_LENGTH 30
+#define MISSILEAWAY_WIDTH 3
+#define MISSILEAWAY_HEIGHT 10
+
+#endif
+
